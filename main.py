9683c3bcdd411c632bf1e6e875794ee4cb6c96a7
Jason Ho <git@hogt.me>
2023-12-04 13:48:51 -0800

    Move logic to fleet.py

--- main.py
+++ main.py
@@ -1,66 +1,59 @@
+import asyncio
+# Import SITL
+# Import DroneKit-Python
+from dronekit import connect, Vehicle
+# Import our classes
+import src.connection as connection_handler
+import src.vehicle as vehicle_handler
+import src.fleet as fleet_handler
 
 RUN_SIMULATOR = False
+def get_connection() -> connection_handler.ConnectionManager:
+    connection = connection_handler.ConnectionManager()
+    if RUN_SIMULATOR:
+        print("Start simulator (SITL)")
+        connection.start_sim()
+    else:
+        connection.to("udpin:10.1.2.3:14550")
+    return connection
 
-import dronekit_sitl
-sitl = None
-connection_string = None
 
-if RUN_SIMULATOR:
-    print("Start simulator (SITL)")
-    sitl = dronekit_sitl.start_default()
-    connection_string = sitl.connection_string()
-else:
-    connection_string = "udpin:10.1.2.3:14550"
-
-import time, sys
-# Import DroneKit-Python
-from dronekit import connect, Vehicle, VehicleMode, LocationGlobalRelative, LocationGlobal
-import src.vehicle as vehicle_handler
 
 _READY_ATTRIBUTES = ['gps_0', 'armed', 'mode', 'attitude'] # modified for MissionPlanner simulator
+def make_vehicle(connection: connection_handler.ConnectionManager) -> Vehicle:
+    # Connect to the Vehicle.
+    print("Connecting to vehicle on: %s" % (connection.connection_string,))
 
-# Connect to the Vehicle.
-print("Connecting to vehicle on: %s" % (connection_string,))
-def makeVehicle(handler):
-    instance = Vehicle(handler)
-    print("Created Vehicle instance")
-    instance._default_ready_attrs = _READY_ATTRIBUTES
-    return instance
+    def makeVehicle(handler):
+        instance = Vehicle(handler)
+        print("Created Vehicle instance")
+        instance._default_ready_attrs = _READY_ATTRIBUTES
+        return instance
 
-veh = connect(connection_string, baud=4800, wait_ready=True, vehicle_class=makeVehicle)
-print("Connected.")
-
-wrapper = vehicle_handler.VehicleWrapper(veh)
+    return connect(connection.connection_string, baud=4800, wait_ready=True, vehicle_class=makeVehicle)
 
 
-def pre_arm(wrapper):
-    if wrapper.is_in_flight():
-        print("Resetting drone...")
-        wrapper.routine_reset()
-        print("Reset!")
-    wrapper.disarm()
+async def main():
+    print("Starting up...")
+    fleet = fleet_handler.Fleet()
 
+    print("Connecting vehicle(s)...")
+    connection_details = get_connection()
+    vehicle = make_vehicle(connection_details)
+    wrapper = vehicle_handler.VehicleWrapper(vehicle)
+    wrapper.routine_preflight() # reset vehicle if flying with preflight routine
+    fleet.fleet_add(wrapper)
+    print("Connected.")
 
-def vehicle_loop(vehicle):
-    print("Pre-flight...")
-    pre_arm(wrapper)
+    print("Running fleet!")
+    fleet.run()
+    print("Fleet closed.")
 
-    input('Preflight complete, take off (enter)')
-    try:
-        print("Arming and taking off...")
-        wrapper.routine_takeoff(20)
-        while True:
-            wrapper.flash_info()
-    except KeyboardInterrupt:
-        print("Exiting info!")
+    wrapper.close()
+    if connection_details.sitl is not None:
+        connection_details.sitl.stop()
+    print("Completed")
 
+asyncio.run(main())
 
-vehicle_loop(veh)
-
-# Close vehicle object before exiting script
-veh.close()
-
-# Shut down simulator
-if RUN_SIMULATOR:
-    sitl.stop()
-print("Completed")
+print("Completed!")
--- src/fleet.py
+++ src/fleet.py
@@ -1,4 +1,5 @@
-from vehicle import VehicleWrapper
+from src.vehicle import VehicleWrapper
+
 class Fleet:
     def __init__(self):
         self._vehicles: list[VehicleWrapper] = []
@@ -9,3 +10,23 @@ class Fleet:
     def takeoff(self, targetAlt = 10):
         for vehicle in self._vehicles:
             vehicle.takeoff(10)
+
+    def tick(self) -> bool:
+        print("Pre-flight...")
+        veh = self._vehicles[0]
+        veh.routine_preflight()
+
+        input('Preflight complete, take off (enter)')
+        try:
+            print("Arming and taking off...")
+            veh.routine_takeoff(20)
+            while True:
+                veh.flash_info()
+        except KeyboardInterrupt:
+            print("Exiting info!")
+        return False
+
+    def run(self):
+        while True:
+            if self.tick():
+                break
