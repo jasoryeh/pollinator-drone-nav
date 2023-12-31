23419dce13649c53dd4cd5dac3dc23f415fb2dd1
Jose Diaz <josediaz@Joses-MacBook-Pro.local>
2023-12-30 17:02:42 -0800

    Line 11 changed details in comments

--- src/connection.py
+++ src/connection.py
@@ -8,10 +8,14 @@ class ConnectionManager:
     def start_sim(self) -> None:
         print("Start simulator (SITL)...")
         self.sitl = dronekit_sitl.start_default()
-        self.connection_string = sitl.connection_string()
+        self.connection_string = self.sitl.connection_string()
         print("Simulator started.")
 
     def to(self, to) -> None:
         self.sitl = None
         self.connection_string = to
+
+        #Modifications
+        #line 11: changed sitl.connection_string() to self.sitl.connection_string()
+        
 
